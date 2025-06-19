47438126dff7dcc1bc4c6b979aeaacd28f9656c2
sudiptoNayek00 <sudipto.nayek777@gmail.com>
2025-06-19 08:53:50 +0530

    Update auth.js

--- src/util/auth.js
+++ src/util/auth.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const isAuthenticated = async ()=>{
     try{
-        const res = await axios.get("http://localhost:5000/api/client/verify",{withCredentials:true});
+        const res = await axios.get("https://zerodha-backend-1-lr1u.onrender.com/api/client/verify",{withCredentials:true});
         console.log("auth - ",res.data);
         return res.data.user
     }catch(err){
